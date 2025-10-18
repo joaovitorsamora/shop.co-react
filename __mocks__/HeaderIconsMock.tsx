0b578395e8f5e0c6d0f70abbc16e5b3b65562703
joaovitorsamora <jvsamora00@gmail.com>
2025-10-18 20:11:26 -0300

    test: create HeaderIconsMock.tsx

---
+++ __mocks__/HeaderIconsMock.tsx
@@ -0,0 +1,7 @@
+import { Menu, Search, ShoppingBag, User } from 'lucide-react';
+
+export const mockHeaderIcons = [
+  { icon: <Search className="flex md:hidden" />, href: '#' },
+  { icon: <ShoppingBag />, href: '#' },
+  { icon: <User />, href: '#' },
+];
