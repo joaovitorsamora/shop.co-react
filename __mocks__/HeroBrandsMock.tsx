d09c72bc61802534ed323dccae2127faee3f6ef4
joaovitorsamora <jvsamora00@gmail.com>
2025-10-18 20:11:32 -0300

    test: create HeroBrandsMock.tsx

---
+++ __mocks__/HeroBrandsMock.tsx
@@ -0,0 +1,7 @@
+export const heroBrandsMock = [
+  { logo: './assets/versace-logo.png', alt: 'Logo da Versace' },
+  { logo: './assets/zara-logo.png', alt: 'Logo da Zara' },
+  { logo: './assets/gucci-logo.png', alt: 'Logo da Gucci' },
+  { logo: './assets/prada-logo.png', alt: 'Logo da Prada' },
+  { logo: './assets/calvin-klein-logo.png', alt: 'Logo da Calvin Klein' },
+];
