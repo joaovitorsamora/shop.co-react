0615d369ea92a02a3ffdaf90d2eeebc9b87e1a13
joaovitorsamora <jvsamora00@gmail.com>
2025-10-18 20:11:16 -0300

    test: create DressStyleMock.tsx

---
+++ __mocks__/DressStyleMock.tsx
@@ -0,0 +1,30 @@
+export const mockClothesStyles = [
+  {
+    image: './assets/casual.png',
+    categoria: 'casual',
+    alt: 'Homem usando roupa casual',
+    href: '#',
+    area: 'casual' as const,
+  },
+  {
+    image: './assets/formal.png',
+    categoria: 'formal',
+    alt: 'Homem usando roupa formal',
+    href: '#',
+    area: 'formal' as const,
+  },
+  {
+    image: './assets/party.png',
+    categoria: 'party',
+    alt: 'Mulher usando roupa para festa',
+    href: '#',
+    area: 'party' as const,
+  },
+  {
+    image: './assets/gym.png',
+    categoria: 'gym',
+    alt: 'Homem usando roupa para academia',
+    href: '#',
+    area: 'gym' as const,
+  },
+];
