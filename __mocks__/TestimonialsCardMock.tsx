1039d0f871cac3e3a274f14078a8c05a8bc8d827
joaovitorsamora <jvsamora00@gmail.com>
2025-10-18 20:11:53 -0300

    test: create TestimonialsCardMock.tsx

---
+++ __mocks__/TestimonialsCardMock.tsx
@@ -0,0 +1,17 @@
+export const mockTestimonialsCard = {
+  id: 1,
+  name: 'Fake name',
+  verificationImage: 'fake-image.png',
+  quote: 'Fake quote...',
+  starsReview: 'fake-image-verification.png',
+};
+
+export const mockTestimonialsCardFromCarrousel = [
+  {
+    id: 1,
+    name: 'Fake name',
+    verificationImage: 'fake-image.png',
+    quote: 'Fake quote...',
+    starsReview: 'fake-image-verification.png',
+  },
+];
