6195c751bcafc9031abc212e5d61ee341f6e332c
joaovitorsamora <jvsamora00@gmail.com>
2025-10-18 20:12:14 -0300

    update HeaderIcons.tsx

--- src/components/HeaderIcons.tsx
+++ src/components/HeaderIcons.tsx
@@ -1,17 +1,21 @@
-import React from "react"
+import React from 'react';
 
-interface HeaderIconsProps { 
-    icons?: Array<{icon: React.ReactNode, href: string}>;
+interface HeaderIconsProps {
+  icons?: Array<{ icon: React.ReactNode; href: string }>;
 }
 
-export const HeaderIcons: React.FC<HeaderIconsProps> = ({icons}) => { 
-    return (
-       <nav className="header__nav">
-            <ul className="flex gap-3">
-                {icons?.map((icon, index) => (
-                    <li className="header__nav-item"><a key={icon.href || index} className="header__nav-link" href={icon.href}>{icon.icon}</a></li>
-                ))}
-            </ul>
-        </nav> 
-    )
-}
+export const HeaderIcons: React.FC<HeaderIconsProps> = ({ icons }) => {
+  return (
+    <nav className="header__nav">
+      <ul className="flex gap-3">
+        {icons?.map((icon, index) => (
+          <li className="header__nav-item" key={index}>
+            <a className="header__nav-link" href={icon.href} aria-label={`link-${index}`}>
+              {icon.icon}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
