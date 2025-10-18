f54d0ff5ef9323078eb027e4f1224e9390106b18
joaovitorsamora <jvsamora00@gmail.com>
2025-10-18 20:11:46 -0300

    test: create ProductCardMock.tsx

---
+++ __mocks__/ProductCardMock.tsx
@@ -0,0 +1,23 @@
+export const mockProducts = {
+  id: 1,
+  image: 'fake-image.png',
+  price: '99.99',
+  review: 10,
+  starsReview: '5',
+  title: 'Produto Teste',
+  discount: '20%',
+  oldPrice: '129.99',
+};
+
+export const mockProductsFromCarrousel = [
+  {
+    id: 1,
+    image: 'fake-image.png',
+    price: '99.99',
+    review: 10,
+    starsReview: '5',
+    title: 'Produto Teste',
+    discount: '20%',
+    oldPrice: '129.99',
+  },
+];
