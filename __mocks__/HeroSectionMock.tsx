b2947b7f0ae9cb55ace8565118cc0772a58e318f
joaovitorsamora <jvsamora00@gmail.com>
2025-10-18 20:11:37 -0300

    test: create HeroSectionMock.tsx

---
+++ __mocks__/HeroSectionMock.tsx
@@ -0,0 +1,13 @@
+export const stats = [
+  { text: '10k+', description: 'Clientes' },
+  { isDivider: true },
+  { text: '2,000+', description: 'High-Quality Products' },
+  { text: '30,000+', description: 'Happy Customers', fullWidth: true },
+];
+export const brands = [
+  { logo: './assets/versace-logo.png', alt: 'Logo da Versace' },
+  { logo: './assets/zara-logo.png', alt: 'Logo da Zara' },
+  { logo: './assets/gucci-logo.png', alt: 'Logo da Gucci' },
+  { logo: './assets/prada-logo.png', alt: 'Logo da Prada' },
+  { logo: './assets/calvin-klein-logo.png', alt: 'Logo da Calvin Klein' },
+];
