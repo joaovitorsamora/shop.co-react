cb892bfe10318b502b6da5ade1e7ef3cfeaef13b
joaovitorsamora <jvsamora00@gmail.com>
2025-10-18 20:11:42 -0300

    test: create HeroStatsItemMock.tsx

---
+++ __mocks__/HeroStatsItemMock.tsx
@@ -0,0 +1,6 @@
+export const heroStatsItemmock = [
+  { text: '200+', description: 'International Brands' },
+  { isDivider: true },
+  { text: '2,000+', description: 'High-Quality Products' },
+  { text: '30,000+', description: 'Happy Customers', fullWidth: true },
+];
