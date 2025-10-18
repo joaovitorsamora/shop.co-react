433f1dcb5d77851dd81d35394b45c1df424340c2
joaovitorsamora <jvsamora00@gmail.com>
2025-10-18 20:12:25 -0300

    update HeaderTopPromo.tsx

--- src/components/HeaderTopPromo.tsx
+++ src/components/HeaderTopPromo.tsx
@@ -1,16 +1,16 @@
-import { TypographyP } from "./Typography/TypographyP";
+import { TypographyP } from './Typography/TypographyP';
 
 interface HeaderTopPromoProps {
-    textPromo: string;
-    textSignUp: string;
+  textPromo: string;
+  textSignUp: string;
 }
-export const HeaderTopPromo: React.FC<HeaderTopPromoProps> = ({textPromo, textSignUp}) => { 
-    return (
-        <aside className="bg-black w-full py-2.5 px-5 flex flex-row justify-center items-center gap-2.5 text-white text-xs font-medium">
-            <TypographyP text={textPromo} />
-            <a href="#" className="text-white underline">
-                <TypographyP text={textSignUp} />
-            </a>
-        </aside>
-    )
-}
+export const HeaderTopPromo: React.FC<HeaderTopPromoProps> = ({ textPromo, textSignUp }) => {
+  return (
+    <aside className="bg-black w-full py-2.5 px-5 flex flex-row justify-center items-center gap-2.5 text-white text-xs font-medium">
+      <TypographyP text={textPromo} />
+      <button className="text-white underline">
+        <TypographyP text={textSignUp} />
+      </button>
+    </aside>
+  );
+};
