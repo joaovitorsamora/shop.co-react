f9948f38fa1143b63e17f1318f689bc8b85490e0
joaovitorsamora <jvsamora00@gmail.com>
2025-10-19 11:39:17 -0300

    refactor: clean up Header component by removing unused icon props

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -1,34 +1,38 @@
-import { HeaderTopPromo } from "../HeaderTopPromo";
-import { HeaderMenu } from "../HeaderMenu";
-import { HeaderSearch } from "../HeaderSearch";
-import { HeaderIcons } from "../HeaderIcons";
+import { HeaderTopPromo } from '../HeaderTopPromo';
+import { HeaderMenu } from '../HeaderMenu';
+import { HeaderSearch } from '../HeaderSearch';
+import { SidePanel } from '../SidePanel';
+import { Search, User } from 'lucide-react';
 
-interface HeaderProps { 
-    logoText: string;
-    renderIcon?: () => React.ReactNode;
-    menuLinks?: Array<{ text: string; href: string }>;
-    icons?: Array<{icon: React.ReactNode, href: string}>;
-    placeholder?: string;
-    textPromo: string;
-    textSignUp: string;
-    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+interface HeaderProps {
+  logoText: string;
+  renderIcon?: () => React.ReactNode;
+  menuLinks?: Array<{ text: string; href: string }>;
+  placeholder?: string;
+  textPromo: string;
+  textSignUp: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const Header: React.FC<HeaderProps> = ({logoText, textPromo, textSignUp, renderIcon, menuLinks, icons, placeholder, onChange}) => { 
-    return (
-        <header>
-            <HeaderTopPromo textPromo={textPromo} textSignUp={textSignUp}/>
-        <section className="flex justify-between items-center py-2.5 px-4">
-                <HeaderMenu
-                    logoText={logoText}
-                    renderIcon={renderIcon}
-                    menuLinks={menuLinks}
-                />
-                <HeaderSearch onChange={onChange} placeholder={placeholder} />
-                <HeaderIcons
-                    icons={icons}
-                />
-        </section>
+export const Header: React.FC<HeaderProps> = ({
+  logoText,
+  textPromo,
+  textSignUp,
+  renderIcon,
+  menuLinks,
+  placeholder,
+  onChange,
+}) => {
+  return (
+    <header>
+      <HeaderTopPromo textPromo={textPromo} textSignUp={textSignUp} />
+      <section className="flex justify-between items-center py-2.5 px-4">
+        <HeaderMenu logoText={logoText} renderIcon={renderIcon} menuLinks={menuLinks} />
+        <HeaderSearch onChange={onChange} placeholder={placeholder} />
+        <Search className="flex md:hidden" />
+        <User className="relative left-[96px]" />
+        <SidePanel />
+      </section>
     </header>
-    )
-}
+  );
+};
