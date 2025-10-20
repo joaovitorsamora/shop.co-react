42dc305c97d5299c2835f4024631f1f3f13559b5
joaovitorsamora <jvsamora00@gmail.com>
2025-10-20 08:15:49 -0300

    refactor: remove unused HeaderIcons import and related code from Header test for cleaner code

--- src/components/sections/__tests__/Header.test.tsx
+++ src/components/sections/__tests__/Header.test.tsx
@@ -3,7 +3,6 @@ import { render, screen } from '@testing-library/react';
 import { HeaderTopPromo } from '../../HeaderTopPromo';
 import { HeaderMenu } from '../../HeaderMenu';
 import { HeaderSearch } from '../../HeaderSearch';
-import { HeaderIcons } from '../../HeaderIcons';
 import userEvent from '@testing-library/user-event';
 import { Header } from '../Header';
 import { mockHeaderIcons } from '../../../../__mocks__/HeaderIconsMock';
@@ -21,11 +20,6 @@ describe('Header', () => {
           { text: 'On Sale', href: '#' },
           { text: 'New Arrivals', href: '#' },
           { text: 'Brands', href: '#' },
-        ]}
-        icons={[
-          { icon: <img src={'./assets/pesquisa-icon.png'} alt="Logo" />, href: '#' },
-          { icon: <img src={'./assets/shop-icon.png'} alt="Logo" />, href: '#' },
-          { icon: <img src={'./assets/profile-icon.png'} alt="Logo" />, href: '#' },
         ]}
         textSignUp="Sign Up Now"
         textPromo="Sign up and get 20% off to your first order."
@@ -62,11 +56,8 @@ describe('Header', () => {
           { text: 'New Arrivals', href: '#' },
           { text: 'Brands', href: '#' },
         ]}
-        renderIcon={() => <img src={'./assets/menu-icon.png'} alt="Logo" />}
       />
     );
-
-    render(<HeaderIcons icons={mockHeaderIcons} />);
 
     expect(screen.getByText('SHOP.CO')).toBeInTheDocument;
     expect(screen.getByText('Shop')).toBeInTheDocument;
