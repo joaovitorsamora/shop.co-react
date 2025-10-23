2d5a4b1e6dc76337319c7f48ffc062bbd58c5e6d
joaovitorsamora <jvsamora00@gmail.com>
2025-10-23 16:35:41 -0300

    style(input)
    reduce md:max-w from 577px to 400px to improve responsiveness and alignment

--- src/components/HeaderSearch.tsx
+++ src/components/HeaderSearch.tsx
@@ -7,7 +7,7 @@ export const HeaderSearch: React.FC<HeaderSearchProps> = ({ onChange, placeholde
   return (
     <input
       type="text"
-      className="hidden sm:block md:pt-[12px] md:pr-[26px] md:pb-[12px] md:pl-[40px] md:my-[24px] md:mx-[40px] md:bg-search-icon-bg md:bg-no-repeat md:[background-position-x:12px] md:[background-position-y:50%] md:rounded-[62px] 2xl:w-full lg:w-[45%] md:w-full md:max-w-[577px] text-sm md:outline-none md:[background-size:14px] md:bg-[#F0F0F0]"
+      className="hidden sm:block md:pt-[12px] md:pr-[26px] md:pb-[12px] md:pl-[40px] md:my-[24px] md:mx-[40px] md:bg-search-icon-bg md:bg-no-repeat md:[background-position-x:12px] md:[background-position-y:50%] md:rounded-[62px] 2xl:w-full lg:w-[45%] md:w-full md:max-w-[400px] text-sm md:outline-none md:[background-size:14px] md:bg-[#F0F0F0]"
       placeholder={placeholder}
       onChange={onChange}
     />
