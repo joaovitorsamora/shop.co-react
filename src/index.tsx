0def3db9ced58a753be8423c020794bb2146b59b
joaovitorsamora <jvsamora00@gmail.com>
2025-10-23 16:44:54 -0300

    feat(app)
    register /category-pages/:category route and configure BrowserRouter with future transition support

--- src/index.tsx
+++ src/index.tsx
@@ -6,16 +6,22 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { DefaultPage } from './components/DefaultPage';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
+import { CategoryPage } from './pages/category-pages';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+      }}
+    >
       <Routes>
         <Route path="/" element={<DefaultPage />}>
           <Route index element={<App />} />
         </Route>
         <Route path="/product-details-page/:id" element={<ProductDetailsPage />} />
+        <Route path="/category-pages/:category" element={<CategoryPage />} />
       </Routes>
     </BrowserRouter>
   </Provider>
