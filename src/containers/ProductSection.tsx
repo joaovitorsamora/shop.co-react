8f93bf543c922b60c201230395cb47561075a730
joaovitorsamora <jvsamora00@gmail.com>
2025-10-23 16:37:08 -0300

    refactor(ProductSection)
    allow custom HTML attributes by extending props and applying them to section element

--- src/containers/ProductSection.tsx
+++ src/containers/ProductSection.tsx
@@ -2,7 +2,7 @@ import { TypographyH2 } from '../components/Typography/TypographyH2';
 import { ProductConTainer } from '../components/PoductContainer';
 import { ProductType } from '../types/ProductType';
 
-interface ProductSectionProps {
+interface ProductSectionProps extends React.HTMLAttributes<HTMLElement> {
   title: string;
   btnTitle: string;
   buttonOnClick: () => void;
@@ -14,9 +14,13 @@ export const ProductSection: React.FC<ProductSectionProps> = ({
   buttonOnClick,
   btnTitle,
   products,
+  ...props
 }) => {
   return (
-    <section className="flex flex-col xl1440:mx-[100px] xl1440:mt-[72px] xl1440:mb-[128px]">
+    <section
+      className="flex flex-col xl1440:mx-[100px] xl1440:mt-[72px] xl1440:mb-[128px]"
+      {...props}
+    >
       <div className="text-center pt-0 pb-12 px-4 xl1440:px-16">
         <TypographyH2 text={title} className="text-3xl font-bold" />
       </div>
