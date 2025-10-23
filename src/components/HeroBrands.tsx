6624143f12d7fd1f14a14aba57a250e6f5561392
joaovitorsamora <jvsamora00@gmail.com>
2025-10-23 16:36:22 -0300

    refactor(HeroBrands)
    assign 'brands' id to <ul> and improve code formatting for readability

--- src/components/HeroBrands.tsx
+++ src/components/HeroBrands.tsx
@@ -1,15 +1,18 @@
-interface HeroBrandsProps { 
-    brands?: Array<{ logo: string; alt: string }>;
+interface HeroBrandsProps {
+  brands?: Array<{ logo: string; alt: string }>;
 }
 
-export const HeroBrands: React.FC<HeroBrandsProps> = ({brands}) => { 
-    return (
-        <ul className="flex flex-wrap justify-center lg:justify-around gap-5 items-center py-5 px-4 bg-black mb-12">
-            {brands?.map((brand) => (
-                <li className="text-white white-space-nowrap" key={brand.alt}>
-                    <img src={brand.logo} alt={brand.alt} />
-                </li>
-            ))}
-            </ul>
-    ) 
-}
+export const HeroBrands: React.FC<HeroBrandsProps> = ({ brands }) => {
+  return (
+    <ul
+      id="brands"
+      className="flex flex-wrap justify-center lg:justify-around gap-5 items-center py-5 px-4 bg-black mb-12"
+    >
+      {brands?.map(brand => (
+        <li className="text-white white-space-nowrap" key={brand.alt}>
+          <img src={brand.logo} alt={brand.alt} />
+        </li>
+      ))}
+    </ul>
+  );
+};
