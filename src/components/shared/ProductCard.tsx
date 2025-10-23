947a18e9b2419a473a2edbc0a85c2e365cdf2950
joaovitorsamora <jvsamora00@gmail.com>
2025-10-23 16:29:26 -0300

    refactor(TestimonialsCard)
    update prop to starsReviewImage to align with TestimonialTypes interface

--- src/components/shared/ProductCard.tsx
+++ src/components/shared/ProductCard.tsx
@@ -1,35 +1,30 @@
 import { ProductType } from '../../types/ProductType';
 import { Link } from 'react-router-dom';
 
-interface ProductCardProps {
-  products: ProductType;
-}
-
-export const ProductCard: React.FC<ProductCardProps> = ({ products }) => {
+export const ProductCard = ({ product }: { product: ProductType }) => {
   const productDetailPage = process.env.REACT_APP_PRODUCT_DETAIL_PAGE;
-
   return (
-    <Link to={`/${productDetailPage}/${products.id}`}>
+    <Link to={`/${productDetailPage}/${product.id}`}>
       <li className="flex gap-3 snap-center min-w-[295px]">
-        <article className="m-auto" key={products.id}>
-          <img className="w-[295px] h-[297px]" src={products.image} alt={products.title} />
+        <article className="m-auto" key={product.id}>
+          <img className="w-[295px] h-[297px]" src={product.image} alt={product.title} />
           <h4 className="text-base font-bold pt-2" data-testid="mock-product">
-            {products.title}
+            {product.title}
           </h4>
           <div
             className="flex items-center gap-3 py-1 px-0"
             role="img"
-            aria-label={`Avaliação: ${products.review} de 5 estrelas`}
+            aria-label={`Avaliação: ${product.review} de 5 estrelas`}
           >
-            <img src={products.starsReview} alt="Stars1" aria-hidden="true" />
-            <p className="text-xs text-gray-500">{products.review}/5</p>
+            <img src={product.starsReviewImage} alt="Stars1" aria-hidden="true" />
+            <p className="text-xs text-gray-500">{product.review}/5</p>
           </div>
           <div className="flex items-center gap-2">
-            <p className="text-xl font-bold text-black">${products.price}</p>
-            {products.oldPrice && (
-              <span className="line-through text-sm text-gray-400">${products.oldPrice}</span>
+            <p className="text-xl font-bold text-black">${product.price}</p>
+            {product.oldPrice && (
+              <span className="line-through text-sm text-gray-400">${product.oldPrice}</span>
             )}
-            {products.discount && <span className="text-sm text-red-500">{products.discount}</span>}
+            {product.discount && <span className="text-sm text-red-500">{product.discount}</span>}
           </div>
         </article>
       </li>
