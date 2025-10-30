10a696ae6a3d78b706be5d7ea05f6ed5117715d7
joaovitorsamora <jvsamora00@gmail.com>
2025-10-30 17:47:30 -0300

    chore:
    replace package-lock.json with pnpm-lock.yaml after migration

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,21 @@ module.exports = {
         poppins: ['Poppins', 'sans-serif'],
       },
     },
+    keyframes: {
+      'slide-up': {
+        '0%': {
+          transform: 'translateY(100%)',
+          opacity: '0',
+        },
+        '100%': {
+          transform: 'translateY(0)',
+          opacity: '1',
+        },
+      },
+    },
+    animation: {
+      'slide-up': 'slide-up 0.5s ease-out forwards',
+    },
   },
   plugins: [],
 };
